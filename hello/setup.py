4f1c7dc069040c17ae8865126332179ffbe07ba2
home <>
2018-08-12 18:15:23 +0800

    add comments

--- hello/setup.py
+++ hello/setup.py
@@ -1,25 +1,18 @@
 from distutils.core import setup,Extension
-from Cython.Build import cythonize
+#  from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy
 
-#  setup(
-        #  ext_modules = cythonize("test.pyx"),
-        #  include_dirs=[numpy.get_include()]
-        #  )
-#  ext_modules = [Extension("pyhello", ["pyhello.pyx"],
-                         #  extra_objects=["hello.o"])]
 
 ext_modules = [Extension("pyhello", ["pyhello.pyx"],
                          #  include_dirs = [npy_include_dir],
                          extra_objects=["hello.o"],
+                         # need libgfortran for fortran write function
                          libraries = ['gfortran'],
                          library_dirs = ['/usr/local/lib'])
                          ]
 setup(
-        #  ext_modules = cythonize("test.pyx",include_path = [numpy.get_include()])
-        #  ext_modules = cythonize(Extension("test",["test.pyx"], include_dirs=[numpy.get_include()]))
-        #  ext_modules = cythonize("hello.pyx")
+        # need line below to carry out compiling, instead of using cythonize 
         cmdclass = {'build_ext': build_ext},
         ext_modules = ext_modules
         )
